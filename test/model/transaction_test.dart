8cfaad1bb22e6e154dda3f16c05b4a5a186f59a2
Noel Dupuis <noel.dupuis45370@gmail.com>
2022-12-02 20:58:13 +0100

    Add test for model creation rules Constructor data should be found in created object No comment should provide empty string as comment

--- test/model/transaction_test.dart
+++ test/model/transaction_test.dart
@@ -10,4 +10,36 @@ void main() {
         amount: 3.14,
         category: "Test Category");
   });
+
+  test('Constructor creates a transaction with provided data', () async {
+    DateTime transactionCreation = DateTime.now();
+
+    Transaction transaction = Transaction(
+        date: transactionCreation,
+        type: TransactionType.EXPENSE,
+        comment: "This is a comment",
+        amount: 3.14,
+        category: "Test Category");
+    expect(transaction.amount, equals(3.14));
+    expect(transaction.type, equals(TransactionType.EXPENSE));
+    expect(transaction.comment, equals("This is a comment"));
+    expect(transaction.date, equals(transactionCreation));
+    expect(transaction.category, equals("Test Category"));
+  });
+
+  test('Constructor: no comment should init the comment to empty string',
+      () async {
+    DateTime transactionCreation = DateTime.now();
+
+    Transaction transaction = Transaction(
+        date: transactionCreation,
+        type: TransactionType.EXPENSE,
+        amount: 3.14,
+        category: "Test Category");
+    expect(transaction.amount, equals(3.14));
+    expect(transaction.type, equals(TransactionType.EXPENSE));
+    expect(transaction.comment, isEmpty);
+    expect(transaction.date, equals(transactionCreation));
+    expect(transaction.category, equals("Test Category"));
+  });
 }
